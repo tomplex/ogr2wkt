1aa32fb1fb2baad9c755d5cb395e9036301d35ee
Tom Caruso <carusot42@gmail.com>
2020-03-23 18:25:05 -0400

    cleanup and add entrypoint to setup.py

--- ogr2wkt/cli.py
+++ ogr2wkt/cli.py
@@ -5,11 +5,6 @@ from ogr2wkt.util import convert_to_wkt
 
 
 @click.command()
-def cli():
-    pass
-
-
-@cli.command()
 @click.argument('input_file')
 @click.argument('output_file')
 def ogr2wkt(input_file, output_file):
